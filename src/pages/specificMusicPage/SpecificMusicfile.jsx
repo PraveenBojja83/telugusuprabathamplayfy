c5cbbc7d4a6ab98ece07341fa5e9d1ac5c8abee7
PraveenBojja83 <Praveen.bojja1983@hotmail.com>
2025-11-22 21:36:40 +0530

    Add initial download option  project files

--- src/pages/specificMusicPage/SpecificMusicfile.jsx
+++ src/pages/specificMusicPage/SpecificMusicfile.jsx
@@ -6,8 +6,8 @@ const SpecificMusicfile = ({currentEle}) => {
         <img src = {currentEle.imgSrc} alt="" style={{ width: "200px", height: "auto" }}/>
         <h2>{currentEle.heading}</h2>
         <p>{currentEle.subHeading}</p>
-        <audio src={currentEle.musicFile} autoPlay controls controlsList="nodownload"></audio>
-
+        <audio src={currentEle.musicFile} autoPlay controls></audio>
+        
     </div>
   )
 }
